a61bf2ad711f89a6e045af56ce16d8f4414142ae
金正朋也 <mhtnim1023@gmail.com>
2019-09-28 15:09:37 +0900

    ドメインモデル実装

---
+++ crawler/src/main/scala/domain/model/ShopInfo.scala
@@ -0,0 +1,7 @@
+package domain.model
+
+case class ShopInfo(
+                 name: Option[String] = None,
+                 siteLink: Option[String] = None,
+                 phoneNumber: Option[String] = None
+               )
