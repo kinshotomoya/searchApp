ff543eefa34458d6d09178fd4db2eaa97a9d6247
金正朋也 <mhtnim1023@gmail.com>
2019-09-28 15:05:44 +0900

    Jsoupを使ってクローラー作る

---
+++ crawler/src/main/scala/crawler/WebCrawler.scala
@@ -0,0 +1,43 @@
+import domain.model.ShopInfo
+import org.jsoup.Jsoup
+import service.ShopService
+
+import scala.collection.JavaConverters._
+
+
+object WebCrawler {
+  def  main(args: Array[String]) = {
+    // 店舗名・リンク先を取得する
+    // case classに詰めて、elasticseachに処理を投げる
+    val urlStr = "http://www.goldsgym.jp"
+    try {
+      val siteHtml = Jsoup.connect(urlStr).get
+      siteHtml.select(".hogeclass#hogeid")
+
+      val shopItems = siteHtml.getElementsByClass("shop-list-item-container").select("[title]").asScala
+
+      val shops: Seq[ShopInfo] = shopItems.map { shop =>
+        ShopInfo(
+          name = Option(shop.attr("title").changeEmptyStrIntoNull),
+          siteLink = Option(urlStr + shop.attr("href").changeEmptyStrIntoNull),
+          phoneNumber = Option(shop.attr("phoneNumber").changeEmptyStrIntoNull)
+        )
+      }
+      ShopService().insertShopInfo(shops)
+
+    } catch {
+      case e:Exception => println(e.getMessage)
+    }
+  }
+
+  implicit class StrSchanger(str: String) {
+    // ""はNoneにするために実装
+    def changeEmptyStrIntoNull = {
+      if(str == "") null else str
+    }
+  }
+}
+
+
+
+
