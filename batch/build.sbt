255bd606c1f8725a883cef206fbc9f3bb633a5b2
金正朋也 <mhtnim1023@gmail.com>
2019-09-30 00:51:42 +0900

    redisとmysqlの疎通確認 - redisclient - doobie

---
+++ batch/build.sbt
@@ -0,0 +1,9 @@
+scalaVersion := "2.12.7"
+
+libraryDependencies ++= Seq(
+  "org.tpolecat" %% "doobie-core"      % "0.8.2",
+  "org.tpolecat" %% "doobie-hikari"    % "0.8.2",
+  "net.debasishg" %% "redisclient" % "3.10",
+  "com.typesafe" % "config" % "1.3.4",
+  "mysql" % "mysql-connector-java" % "5.1.24"
+)
---
+++ batch/src/main/scala/Test.scala
@@ -0,0 +1,48 @@
+import com.redis.RedisClient
+import cats._
+import cats.data._
+import cats.implicits._
+import doobie._
+import doobie.implicits._
+import cats._
+import cats.effect._
+import cats.implicits._
+import doobie.util.ExecutionContexts
+import doobie.util.transactor.Transactor.fromDriverManager
+// ConfigFactoryを使うと、.confで書いた設定ファイルをアプリケーションに読み込むことができる。
+import com.typesafe.config.ConfigFactory
+
+
+
+object Test {
+  def main(args: Array[String]): Unit = {
+    // redisの使い方
+//    val redisClient = new RedisClient("localhost", 6379)
+//    redisClient.set("ranking", Seq("tomoya", "nana", "ikuya", "mimi"))
+//    val maybeString = redisClient.get("ranking")
+//    println(maybeString)
+
+    // doobieの使い方
+    case class Person(name: String, age: Int)
+    val nel = NonEmptyList.of(Person("tomoya", 24), Person("nana", 22))
+
+    val program1 = 42.pure[ConnectionIO]
+    implicit val cs = IO.contextShift(ExecutionContexts.synchronous)
+    val config = ConfigFactory.load("database")
+    val driver = config.getString("database.driver")
+    val url =  config.getString("database.url")
+    val user =  config.getString("database.user")
+    val password =  config.getString("database.password")
+    val xa = Transactor.fromDriverManager[IO](
+      driver,
+      url,
+      user,
+      password,
+      // 同期的にしている
+      Blocker.liftExecutionContext(ExecutionContexts.synchronous)
+    )
+    val io = program1.transact(xa)
+    println(io.unsafeRunSync)
+
+  }
+}
